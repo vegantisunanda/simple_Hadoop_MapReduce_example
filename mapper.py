2252f73264c794c4b37ee94044bb545ba2ac52ac
vegantisunanda <91168502+vegantisunanda@users.noreply.github.com>
2022-01-26 16:04:01 -0700

    Update mapper.py

--- mapper.py
+++ mapper.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python
 import sys
-
+    
 # get all lines from stdin
 for line in sys.stdin:
     # remove leading and trailing whitespace
-    line = line.strip()
+    line = line.strip().lower()
 
     # split the line into words; splits on any whitespace
     words = line.split()
 
     # output tuples (word, 1) in tab-delimited format
-       stopwords = set(['the', 'and'])
+    stop_words = set(['the', 'and'])
         
     for word in words: 
-        if word not in stopwords: 
-            print word
+        if word not in stop_words: 
+           print (word)
